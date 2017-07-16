74acaf5e3a0b22ef23fd799260517a0e8b6b8081
ShimabukuroYuki <shima.nobishiro@gmail.com>
2017-07-17 02:06:09 +0900

    animation

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function(){
 	});
 
 //画像にマウスを乗せたら発動
-  $('img').hover(function() {
+  $('#img').hover(function() {
  
     //画像のsrc属性が別画像のパスに切り替わる
     $(this).attr('src', 'image/logo.jpg');
@@ -16,6 +16,21 @@ $(function(){
     $(this).attr('src', 'image/logo-blackandwhite.jpg');
  
   });
+
+  $('#top').addClass('slideInLeft');
+
+  setTimeout(function(){
+    $('.logo').addClass('fadeIn');
+  },1500);
+
+
+  setTimeout(function(){
+     $('#top').removeClass('slideInLeft');
+     $('#top').addClass('slideOutLeft');
+  },3000);
+
+  
+
 
 
 });
