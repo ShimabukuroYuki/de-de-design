eb095629ee5f9c56e9a5d3f1ba217a33931cd646
yukidallas <yukidallas@gmail.com>
2017-07-17 02:31:42 +0900

    Cleanup

--- js/main.js
+++ js/main.js
@@ -3,34 +3,23 @@ $(function(){
 	  itemSelector: '.grid-item'
 	});
 
-//画像にマウスを乗せたら発動
   $('#img').hover(function() {
- 
-    //画像のsrc属性が別画像のパスに切り替わる
     $(this).attr('src', 'image/logo.jpg');
- 
-  //ここにはマウスを離したときの動作を記述
   }, function() {
- 
-    //画像のsrc属性を元の画像のパスに戻す
     $(this).attr('src', 'image/logo-blackandwhite.jpg');
- 
   });
 
-  $('#top').addClass('slideInLeft');
+  function slideAnimation(){
+    $('.top-slide-container').addClass('animated slideInLeft');
+    setTimeout(function(){
+      $('.top-slide-logo').addClass('animated fadeIn');
+    },1500);
 
-  setTimeout(function(){
-    $('.logo').addClass('fadeIn');
-  },1500);
+    setTimeout(function(){
+       $('.top-slide-container').removeClass('slideInLeft');
+       $('.top-slide-container').addClass('slideOutLeft');
+    },3000);
+  }
 
-
-  setTimeout(function(){
-     $('#top').removeClass('slideInLeft');
-     $('#top').addClass('slideOutLeft');
-  },3000);
-
-  
-
-
-
+  slideAnimation()
 });
