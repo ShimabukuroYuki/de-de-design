acfaca799ae92cf94257fd65416ae34d1a8ddda7
ShimabukuroYuki <shima.nobishiro@gmail.com>
2017-07-25 23:34:47 +0900

    remove comment

--- js/main.js
+++ js/main.js
@@ -35,9 +35,7 @@ $(function(){
       var href = $(this).attr("href");
       // 移動先を取得
       var target = $(href == "#" || href == "" ? 'html' : href);
-      // 移動先を数値で取得
       var position = target.offset().top;
-      // スムーススクロール
       $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
    });
